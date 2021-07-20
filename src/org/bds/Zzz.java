8c04b2f7709bb6174dbaf29d68a9b3e0fa57837d
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2021-07-20 16:07:19 -0400

    Project updated
    Former-commit-id: 6eadfac11a6f0a60f43fe31f0fb146e23b8d1ea0
    Former-commit-id: 7991b55b084a1f22811a41f1f35e5f3cf3402618

--- src/org/bds/Zzz.java
+++ src/org/bds/Zzz.java
@@ -1,40 +1,40 @@
 package org.bds;
 
-import org.bds.executioner.QueueThreadAwsSqs;
-import org.bds.osCmd.CmdAws;
-import org.bds.test.BdsTest;
 import org.bds.util.Gpr;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Map;
 
 public class Zzz {
 
-	public static boolean debug = true;
-	public static boolean log = true;
-	public static boolean verbose = true;
+    public static boolean debug = true;
+    public static boolean log = true;
+    public static boolean verbose = true;
 
-	public static final String QUEUE_NAME = "bds_test_123456789";
+    public static void main(String[] args) throws Exception {
+        System.out.println("Start");
 
-	// Read bucket name from $HOME/.bds/aws_test_bucket.txt
-	public static String awsBucketName() {
-		String awsBucketNameFile = Gpr.HOME + "/.bds/aws_test_bucket.txt";
-		String bucket = Gpr.readFile(awsBucketNameFile);
-		bucket = bucket.split("\n")[0].trim();
-		if (verbose) System.out.println("Bucket name: " + bucket);
-		return bucket;
-	}
+        var fileName = Gpr.HOME + "/z.json";
+        System.out.println(fileName);
+        try (InputStream is = new FileInputStream(new File(fileName));
+             JsonReader rdr = Json.createReader(is)) {
+            // JsonStructure stuct = rdr.read();
+//            System.out.println("STRUCT: " + stuct.getValueType());
+//            System.out.println("STRUCT: " + stuct);
+            JsonObject jobj = rdr.readObject();
+            System.out.println("OBJ: " + jobj);
+            for (Map.Entry<String, JsonValue> e : jobj.entrySet()) {
+                var val = e.getValue();
+                System.out.println(val.getValueType() + "\t" + e);
+            }
+        }
 
-	public static void main(String[] args) {
-		System.out.println("Start");
-
-		// Do not run instance
-		CmdAws.DO_NOT_RUN_INSTANCE = true;
-		QueueThreadAwsSqs.USE_QUEUE_NAME_DEBUG = true;
-
-		String fileName = "test/z.bds";
-
-		BdsTest bdsTest = new BdsTest(fileName, verbose, debug);
-		bdsTest.setLog(log);
-		bdsTest.run();
-
-		System.out.println("End");
-	}
+        System.out.println("End");
+    }
 }
