51e41d6e0b7a61676daf329a40496491617d04a4
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2021-07-31 13:55:58 -0400

    Project updated

--- scripts/distro.sh
+++ scripts/distro.sh
@@ -5,11 +5,15 @@ echo "Distro: Start "
 SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
 source "$SCRIPT_DIR/config.sh"
 
+# First parameter is 'OS', if missing use `uname`
+OS=${1:-$(uname)}
+echo "OS: '$OS'"
+
 # Install files
 "$SCRIPT_DIR/install.sh"
 
 mkdir -p "$DISTRO_DIR"
 cd "$BDS_HOME"
-tar -cvzf "$DISTRO_DIR/bds_$(uname).tar.gz" bds bds.config include clusterGeneric
+tar -cvzf "$DISTRO_DIR/bds_$OS.tar.gz" bds bds.config include clusterGeneric
 
 echo "Distro: End"
