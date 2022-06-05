653f8e6823fae8094b7a4fe8612cb02a115e6566
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2022-06-05 16:21:29 -0400

    Project updated

---
+++ vscode/bds/syntaxes/use_main.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm bds.tmLanguage.json; ln -s bds.tmLanguage.main.json bds.tmLanguage.json
---
+++ vscode/bds/syntaxes/use_test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm bds.tmLanguage.json; ln -s bds.tmLanguage.test.json bds.tmLanguage.json
