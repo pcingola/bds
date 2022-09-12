cebf6cde1fd2a4d22bd65d6ff9f284b70c0123ab
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2022-09-12 14:38:37 -0400

    Project updated

--- src/org/bds/lang/statement/Throw.java
+++ src/org/bds/lang/statement/Throw.java
@@ -9,6 +9,7 @@ import org.bds.lang.nativeClasses.exception.ClassDeclarationException;
 import org.bds.lang.type.Type;
 import org.bds.lang.type.TypeClass;
 import org.bds.symbol.SymbolTable;
+import org.bds.vm.OpCode;
 
 /**
  * Throw statement
@@ -17,58 +18,58 @@ import org.bds.symbol.SymbolTable;
  */
 public class Throw extends StatementWithScope {
 
-	private static final long serialVersionUID = -861450592187243401L;
+    private static final long serialVersionUID = -861450592187243401L;
 
-	Expression expr;
+    Expression expr;
 
-	public Throw(BdsNode parent, ParseTree tree) {
-		super(parent, tree);
-	}
+    public Throw(BdsNode parent, ParseTree tree) {
+        super(parent, tree);
+    }
 
-	/**
-	 * Is the expression derived from 'Exception'?
-	 */
-	boolean isExceptionClass() {
-		Type t = expr.getReturnType();
-		if (t == null) return false;
-		if (!t.isClass()) return false;
+    /**
+     * Is the expression derived from 'Exception'?
+     */
+    boolean isExceptionClass() {
+        Type t = expr.getReturnType();
+        if (t == null) return false;
+        if (!t.isClass()) return false;
 
-		for (TypeClass tc = (TypeClass) t; tc != null; tc = tc.getClassDeclaration().getClassTypeParent()) {
-			if (tc.getCanonicalName().equals(ClassDeclarationException.CLASS_NAME_EXCEPTION)) return true;
-		}
+        for (TypeClass tc = (TypeClass) t; tc != null; tc = tc.getClassDeclaration().getClassTypeParent()) {
+            if (tc.getCanonicalName().equals(ClassDeclarationException.CLASS_NAME_EXCEPTION)) return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	@Override
-	protected void parse(ParseTree tree) {
-		int idx = 0;
-		if (isTerminal(tree, idx, "throw")) idx++;
-		expr = (Expression) factory(tree, idx);
-	}
+    @Override
+    protected void parse(ParseTree tree) {
+        int idx = 0;
+        if (isTerminal(tree, idx, "throw")) idx++;
+        expr = (Expression) factory(tree, idx);
+    }
 
-	@Override
-	public Type returnType(SymbolTable symtab, CompilerMessages compilerMessages) {
-		if (returnType != null) return returnType;
-		returnType = expr.returnType(symtab, compilerMessages);
-		return returnType;
-	}
+    @Override
+    public Type returnType(SymbolTable symtab, CompilerMessages compilerMessages) {
+        if (returnType != null) return returnType;
+        returnType = expr.returnType(symtab, compilerMessages);
+        return returnType;
+    }
 
-	@Override
-	public String toAsm() {
-		return expr.toAsm() + "throw\n";
-	}
+    @Override
+    public String toAsm() {
+        return expr.toAsm() + OpCode.THROW + "\n";
+    }
 
-	@Override
-	public String toString() {
-		return "throw " + (expr != null ? expr.toString() : "");
-	}
+    @Override
+    public String toString() {
+        return "throw " + (expr != null ? expr.toString() : "");
+    }
 
-	@Override
-	public void typeCheckNotNull(SymbolTable symtab, CompilerMessages compilerMessages) {
-		if (!isExceptionClass()) {
-			compilerMessages.add(this, "Trying to 'throw' a non-Exception object", MessageType.ERROR);
-		}
-	}
+    @Override
+    public void typeCheckNotNull(SymbolTable symtab, CompilerMessages compilerMessages) {
+        if (!isExceptionClass()) {
+            compilerMessages.add(this, "Trying to 'throw' a non-Exception object", MessageType.ERROR);
+        }
+    }
 
 }
--- src/org/bds/lang/statement/Try.java
+++ src/org/bds/lang/statement/Try.java
@@ -2,6 +2,7 @@ package org.bds.lang.statement;
 
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.bds.lang.BdsNode;
+import org.bds.vm.OpCode;
 
 /**
  * try / catch / finally statements
@@ -10,41 +11,41 @@ import org.bds.lang.BdsNode;
  */
 public class Try extends StatementWithScope {
 
-	private static final long serialVersionUID = -2501792284878137707L;
+    private static final long serialVersionUID = -2501792284878137707L;
 
-	Statement statement;
+    Statement statement;
 
-	public Try(BdsNode parent, ParseTree tree) {
-		super(parent, tree);
-	}
+    public Try(BdsNode parent, ParseTree tree) {
+        super(parent, tree);
+    }
 
-	@Override
-	protected void parse(ParseTree tree) {
-		int idx = 0;
-		while (!isTerminal(tree, idx, "try"))
-			idx++;
-		statement = (Statement) factory(tree, ++idx);
-	}
+    @Override
+    protected void parse(ParseTree tree) {
+        int idx = 0;
+        while (!isTerminal(tree, idx, "try"))
+            idx++;
+        statement = (Statement) factory(tree, ++idx);
+    }
 
-	public String toAsm(String finallyLabel) {
-		StringBuilder sb = new StringBuilder();
-		sb.append(super.toAsm());
-		if (statement != null) {
-			if (isNeedsScope()) sb.append("scopepush\n");
-			sb.append(statement.toAsm());
-			if (isNeedsScope()) sb.append("scopepop\n");
-		}
-		sb.append("jmp '" + finallyLabel + "'\n");
-		return sb.toString();
-	}
+    public String toAsm(String finallyLabel) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toAsm());
+        if (statement != null) {
+            if (isNeedsScope()) sb.append(OpCode.SCOPEPUSH + "\n");
+            sb.append(statement.toAsm());
+            if (isNeedsScope()) sb.append(OpCode.SCOPEPOP + "\n");
+        }
+        sb.append(OpCode.JMP + " '" + finallyLabel + "'\n");
+        return sb.toString();
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("try {\n");
-		if (statement != null) sb.append(statement);
-		sb.append("} ");
-		return sb.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("try {\n");
+        if (statement != null) sb.append(statement);
+        sb.append("} ");
+        return sb.toString();
+    }
 
 }
