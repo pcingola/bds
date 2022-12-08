0a4030620c0797e76d0ccff15b506499e2a11b07
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2022-12-08 11:43:46 -0500

    Project updated

--- src/org/bds/test/integration/TestCasesIntegrationRun.java
+++ src/org/bds/test/integration/TestCasesIntegrationRun.java
@@ -1,166 +1,168 @@
 package org.bds.test.integration;
 
+import junit.framework.Assert;
+import org.bds.test.TestCasesBase;
+import org.bds.util.Timer;
+import org.junit.Test;
+
 import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.bds.test.TestCasesBase;
-import org.bds.util.Gpr;
-import org.bds.util.Timer;
-import org.junit.Test;
-
-import junit.framework.Assert;
-
 /**
  * Test cases that require BDS code execution and check results.
  * These "integration" test usually require a bit longer execution times than the unit tests.
- *
+ * <p>
  * Note: These test cases requires that the BDS code is correctly parsed, compiled and executes.
  *
  * @author pcingola
  */
 public class TestCasesIntegrationRun extends TestCasesBase {
 
-	@Test
-	public void test104() {
-		runAndCheck("test/run_104.bds", "isRun", "true");
-	}
+    public TestCasesIntegrationRun() {
+        dir = dir + "test/integration/run/";
+    }
 
-	@Test
-	public void test105() {
-		runAndCheck("test/run_105.bds", "isRun", "false");
-	}
+    @Test
+    public void test104() {
+        runAndCheck(dir + "run_104.bds", "isRun", "true");
+    }
 
-	@Test
-	public void test114_parallel_function_task_calls() {
-		String stdout = runAndReturnStdout("test/run_114.bds");
+    @Test
+    public void test105() {
+        runAndCheck(dir + "run_105.bds", "isRun", "false");
+    }
 
-		Set<String> linesPar = new HashSet<>();
-		for (String line : stdout.split("\n"))
-			if (line.startsWith("TASK")) linesPar.add(line);
+    @Test
+    public void test114_parallel_function_task_calls() {
+        String stdout = runAndReturnStdout(dir + "run_114.bds");
 
-		// Check
-		Assert.assertTrue("There should be 5 tasks", linesPar.size() == 5);
-	}
+        Set<String> linesPar = new HashSet<>();
+        for (String line : stdout.split("\n"))
+            if (line.startsWith("TASK")) linesPar.add(line);
 
-	@Test
-	public void test115_task_dependency_using_taskId() {
-		String stdout = runAndReturnStdout("test/run_115.bds");
-		Assert.assertEquals("Hi 1\nBye 1\nHi 2\nBye 2\n", stdout);
-	}
+        // Check
+        Assert.assertTrue("There should be 5 tasks", linesPar.size() == 5);
+    }
 
-	@Test
-	public void test117_serial_parallel_tasks() {
-		String expectedStdout = "Iter 1, Task 1: End\n" //
-				+ "Iter 1, Task 2: Start\n" //
-				+ "Iter 1, Task 2: End\n" //
-				+ "Iter 5, Task 1: End\n" //
-				+ "Iter 5, Task 2: Start\n" //
-				+ "Iter 5, Task 2: End\n" //
-		;
+    @Test
+    public void test115_task_dependency_using_taskId() {
+        String stdout = runAndReturnStdout(dir + "run_115.bds");
+        Assert.assertEquals("Hi 1\nBye 1\nHi 2\nBye 2\n", stdout);
+    }
 
-        String stdout = runAndReturnStdout("test/run_117.bds");
+    @Test
+    public void test117_serial_parallel_tasks() {
+        String expectedStdout = "Iter 1, Task 1: End\n" //
+                + "Iter 1, Task 2: Start\n" //
+                + "Iter 1, Task 2: End\n" //
+                + "Iter 5, Task 1: End\n" //
+                + "Iter 5, Task 2: Start\n" //
+                + "Iter 5, Task 2: End\n" //
+                ;
 
-		if (stdout.indexOf(expectedStdout) < 0) {
-			String msg = "Cannot find desired output:\n" //
-					+ "---------- Expected output ----------\n" //
-					+ expectedStdout //
-					+ "-------------- STDOUT --------------\n" //
-					+ stdout //
-			;
-			System.err.println(msg);
-			throw new RuntimeException(msg);
-		}
-	}
+        String stdout = runAndReturnStdout(dir + "run_117.bds");
 
-	@Test
-	public void test119_task_dependency() {
+        if (stdout.indexOf(expectedStdout) < 0) {
+            String msg = "Cannot find desired output:\n" //
+                    + "---------- Expected output ----------\n" //
+                    + expectedStdout //
+                    + "-------------- STDOUT --------------\n" //
+                    + stdout //
+                    ;
+            System.err.println(msg);
+            throw new RuntimeException(msg);
+        }
+    }
 
-		// Delete input file
-		String inFile = "tmp_in.txt";
-		(new File(inFile)).delete();
+    @Test
+    public void test119_task_dependency() {
 
-		String expectedStdout1 = "Creating tmp_in.txt\n" //
-				+ "Running task\n" //
-				+ "Creating tmp_out.txt\n" //
-				+ "Done\n" //"
-		;
+        // Delete input file
+        String inFile = "tmp_in.txt";
+        (new File(inFile)).delete();
 
-		String expectedStdout2 = "Running task\n" //
-				+ "Done\n" //"
-		;
+        String expectedStdout1 = "Creating tmp_in.txt\n" //
+                + "Running task\n" //
+                + "Creating tmp_out.txt\n" //
+                + "Done\n" //"
+                ;
 
-		if (verbose) System.out.println("First run:");
-		runAndCheckStdout("test/run_119.bds", expectedStdout1);
+        String expectedStdout2 = "Running task\n" //
+                + "Done\n" //"
+                ;
 
-		if (verbose) System.out.println("\n\nSecond run:");
-		runAndCheckStdout("test/run_119.bds", expectedStdout2);
-	}
+        if (verbose) System.out.println("First run:");
+        runAndCheckStdout(dir + "run_119.bds", expectedStdout1);
 
-	@Test
-	public void test123_literals_task() {
+        if (verbose) System.out.println("\n\nSecond run:");
+        runAndCheckStdout(dir + "run_119.bds", expectedStdout2);
+    }
 
-		String output = "" //
-				// Note: This result may change if we use a different taskShell in bds.config
-				+ "task               |\t|\n" //
-				+ "task               |\t|    variable:Hello\n" //
-				+ "task               |\\t|   variable:Hello\n" //
-		;
+    @Test
+    public void test123_literals_task() {
 
-		runAndCheckStdout("test/run_123_literals_task.bds", output);
-	}
+        String output = "" //
+                // Note: This result may change if we use a different taskShell in bds.config
+                + "task               |\t|\n" //
+                + "task               |\t|    variable:Hello\n" //
+                + "task               |\\t|   variable:Hello\n" //
+                ;
 
-	@Test
-	public void test27() {
-		Timer timer = new Timer();
-		timer.start();
-		runAndCheck(1, "test/run_27.bds", "timeout", "1"); // 2 seconds timeout
-		Assert.assertTrue(timer.elapsed() < 3 * 1000); // We should finish in less than 3 secs (the program waits 60secs)
-	}
+        runAndCheckStdout(dir + "run_123_literals_task.bds", output);
+    }
 
-	@Test
-	public void test30() {
-		runAndCheck("test/run_30.bds", "events", "[runnning, wait, done]");
-	}
+    @Test
+    public void test27() {
+        Timer timer = new Timer();
+        timer.start();
+        runAndCheck(1, dir + "run_27.bds", "timeout", "1"); // 2 seconds timeout
+        Assert.assertTrue(timer.elapsed() < 3 * 1000); // We should finish in less than 3 secs (the program waits 60secs)
+    }
 
-	@Test
-	public void test36() {
-		runAndCheck(1, "test/run_36.bds", "s", "before");
-	}
+    @Test
+    public void test30() {
+        runAndCheck(dir + "run_30.bds", "events", "[runnning, wait, done]");
+    }
 
-	@Test
-	public void test84() {
-		runAndCheck(1, "test/run_84.bds", "taskOk", "false");
-	}
+    @Test
+    public void test36() {
+        runAndCheck(1, dir + "run_36.bds", "s", "before");
+    }
 
-	@Test
-	public void test85() {
-		runAndCheckStderr("test/run_84.bds", "ERROR_TIMEOUT");
-	}
+    @Test
+    public void test84() {
+        runAndCheck(1, dir + "run_84.bds", "taskOk", "false");
+    }
 
-	@Test
-	public void test87() {
-		runAndCheck("test/run_87.bds", "cpus", "1");
-	}
+    @Test
+    public void test85() {
+        runAndCheckStderr(dir + "run_84.bds", "ERROR_TIMEOUT");
+    }
 
-	@Test
-	public void test90() {
-		runAndCheck("test/run_90.bds", "ok", "true");
-	}
+    @Test
+    public void test87() {
+        runAndCheck(dir + "run_87.bds", "cpus", "1");
+    }
 
-	@Test
-	public void test91() {
-		runAndCheck(1, "test/run_91.bds", "ok", "false");
-	}
+    @Test
+    public void test90() {
+        runAndCheck(dir + "run_90.bds", "ok", "true");
+    }
 
-	@Test
-	public void test92() {
-		runAndCheck("test/run_92.bds", "outs", "TASK 1\nTASK 2\n");
-	}
+    @Test
+    public void test91() {
+        runAndCheck(1, dir + "run_91.bds", "ok", "false");
+    }
 
-	@Test
-	public void test93() {
-		runAndCheck("test/run_93.bds", "outs", "TASK 1\nTASK 2\n");
-	}
+    @Test
+    public void test92() {
+        runAndCheck(dir + "run_92.bds", "outs", "TASK 1\nTASK 2\n");
+    }
+
+    @Test
+    public void test93() {
+        runAndCheck(dir + "run_93.bds", "outs", "TASK 1\nTASK 2\n");
+    }
 
 }
