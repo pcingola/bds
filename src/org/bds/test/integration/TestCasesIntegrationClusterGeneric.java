6fc2e066a94754eede430600d3d14be63ac7f5bf
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2022-12-08 17:35:19 -0500

    Project updated

--- src/org/bds/test/integration/TestCasesIntegrationClusterGeneric.java
+++ src/org/bds/test/integration/TestCasesIntegrationClusterGeneric.java
@@ -1,38 +1,40 @@
 package org.bds.test.integration;
 
+import junit.framework.Assert;
 import org.bds.task.Task;
 import org.bds.test.BdsTest;
 import org.bds.test.TestCasesBase;
 import org.bds.util.Gpr;
 import org.junit.Test;
 
-import junit.framework.Assert;
-
 /**
  * Executoner "Cluster Generic".
  * This test the cluster generic using a simple "mock" cluster scripts
  *
  * @author pcingola
- *
  */
 public class TestCasesIntegrationClusterGeneric extends TestCasesBase {
 
-	@Test
-	public void test01_log_TestCasesClusterGeneric() {
-		// Create command line
-		String[] args = { "-c", "test/clusterGeneric_localhost_01.config" };
-		BdsTest bdsTest = new BdsTest("test/clusterGeneric_01.bds", args, verbose, debug);
-		bdsTest.bds(false);
+    public TestCasesIntegrationClusterGeneric() {
+        dir = "test/integration/cluter_generic/";
+    }
 
-		// Run script
-		bdsTest.run();
-		bdsTest.checkRunOk(); // Finished OK?
+    @Test
+    public void test01_log_TestCasesClusterGeneric() {
+        // Create command line
+        String[] args = {"-c", dir + "clusterGeneric_localhost_01.config"};
+        BdsTest bdsTest = new BdsTest(dir + "clusterGeneric_01.bds", args, verbose, debug);
+        bdsTest.bds(false);
 
-		// Get tasks and check that PID matches 'CLUSTERGENERIC_LOCALHOST_'
-		// (run.pl prepends that string to PID)
-		for (Task t : bdsTest.bds.getBdsRun().getBdsThread().getTasks()) {
-			if (debug) Gpr.debug("Task " + t.getId() + ", pid " + t.getPid());
-			Assert.assertTrue("Task " + t.getId() + " was NOT executed by ClusterGeneric_localhos (pid " + t.getPid() + ")", t.getPid().startsWith("CLUSTERGENERIC_LOCALHOST_"));
-		}
-	}
+        // Run script
+        bdsTest.run();
+        bdsTest.checkRunOk(); // Finished OK?
+
+        // Get tasks and check that PID matches 'CLUSTERGENERIC_LOCALHOST_'
+        // (run.pl prepends that string to PID)
+        for (Task t : bdsTest.bds.getBdsRun().getBdsThread().getTasks()) {
+            if (debug) Gpr.debug("Task " + t.getId() + ", pid " + t.getPid());
+            Assert.assertTrue("Task " + t.getId() + " was NOT executed by ClusterGeneric_localhos (pid " + t.getPid() + ")", t.getPid().startsWith("CLUSTERGENERIC_LOCALHOST_"));
+        }
+    }
 }
--- src/org/bds/test/integration/TestCasesIntegrationClusterSsh.java
+++ src/org/bds/test/integration/TestCasesIntegrationClusterSsh.java
@@ -1,12 +1,10 @@
 package org.bds.test.integration;
 
+import junit.framework.Assert;
 import org.bds.task.Task;
 import org.bds.test.BdsTest;
 import org.bds.test.TestCasesBase;
-import org.bds.util.Gpr;
 import org.junit.Test;
-
-import junit.framework.Assert;
 
 /**
  * Cluster "ssh" is a "cluster" formed by nodes to which we have password-less ssh access.
@@ -16,27 +14,31 @@ import junit.framework.Assert;
  */
 public class TestCasesIntegrationClusterSsh extends TestCasesBase {
 
-	/**
-	 * Test a simple command running via 'ssh cluster'
-	 */
-	@Test
-	public void test01() {
-		// Create command line
-		String[] args = { "-c", "test/clusterSsh_localhost_01.config" };
-		BdsTest bdsTest = new BdsTest("test/clusterSsh_01.bds", args, verbose, debug);
-		bdsTest.bds(false);
+    public TestCasesIntegrationClusterSsh() {
+        dir = "test/integration/cluter_ssh/";
+    }
 
-		// Run script
-		bdsTest.run();
-		bdsTest.checkRunOk(); // Finished OK?
+    /**
+     * Test a simple command running via 'ssh cluster'
+     */
+    @Test
+    public void test01() {
+        // Create command line
+        String[] args = {"-c", dir + "clusterSsh_localhost_01.config"};
+        BdsTest bdsTest = new BdsTest(dir + "clusterSsh_01.bds", args, verbose, debug);
+        bdsTest.bds(false);
 
-		// Get tasks and check that PID matches 'CLUSTERGENERIC_LOCALHOST_'
-		// (run.pl prepends that string to PID)
-		for (Task t : bdsTest.bds.getBdsRun().getBdsThread().getTasks()) {
-			debug("Task " + t.getId() + ", pid " + t.getPid());
-			Assert.assertTrue("Task " + t.getId() + " was NOT executed by 'Cluster Ssh', task id " + t.getId() //
-					, t.getId().toUpperCase().startsWith("CLUSTERSSH") //
-			);
-		}
-	}
+        // Run script
+        bdsTest.run();
+        bdsTest.checkRunOk(); // Finished OK?
+
+        // Get tasks and check that PID matches 'CLUSTERGENERIC_LOCALHOST_'
+        // (run.pl prepends that string to PID)
+        for (Task t : bdsTest.bds.getBdsRun().getBdsThread().getTasks()) {
+            debug("Task " + t.getId() + ", pid " + t.getPid());
+            Assert.assertTrue("Task " + t.getId() + " was NOT executed by 'Cluster Ssh', task id " + t.getId() //
+                    , t.getId().toUpperCase().startsWith("CLUSTERSSH") //
+            );
+        }
+    }
 }
