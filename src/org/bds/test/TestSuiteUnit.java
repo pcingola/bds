4132222bfb40bf8516e5c3289ea42d2f9bce74e6
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2022-12-08 05:58:34 -0500

    Project updated

--- src/org/bds/test/TestSuiteUnit.java
+++ src/org/bds/test/TestSuiteUnit.java
@@ -12,21 +12,22 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestCasesTail.class, //
-		TestCasesVm.class, // Virtual machine
-		TestCasesLang.class, // Language (compiler)
-		TestCasesInterpolate.class, // Variable interpolation
-		TestCasesExecutioners.class, // Task executioners
-		TestCasesFunctionDeclaration.class, // Function declaration
-		TestCasesRun.class, // Running bds code
-		TestCasesRun2.class, // Running bds code
-		TestCasesRun3.class, // Running bds code (classes / object)
-		TestCasesCustomResources.class, // Custom resource allocation in tasks (e.g. GPU, FFPGA, etc.)
-		TestCasesJson.class, // JSON parsing
-		TestCasesTesting.class, // Check bds unit testing system
-		TestCasesCommandLineOptions.class, // Check command line options
-		TestCasesRemote.class, // Accessing remote data (cloud storage)
-		TestCasesReport.class, // Report generation
+@SuiteClasses({ //
+        TestCasesTail.class, //
+        TestCasesVm.class, // Virtual machine
+        TestCasesLang.class, // Language (compiler)
+        TestCasesInterpolate.class, // Variable interpolation
+        TestCasesExecutioners.class, // Task executioners
+        TestCasesFunctionDeclaration.class, // Function declaration
+        TestCasesRun.class, // Running bds code
+        TestCasesRun2.class, // Running bds code
+        TestCasesRun3.class, // Running bds code (classes / object)
+        TestCasesCustomResources.class, // Custom resource allocation in tasks (e.g. GPU, FFPGA, etc.)
+        TestCasesJson.class, // JSON parsing
+        TestCasesTesting.class, // Check bds unit testing system
+        TestCasesCommandLineOptions.class, // Check command line options
+        TestCasesRemote.class, // Accessing remote data (cloud storage)
+        TestCasesReport.class, // Report generation
 })
 public class TestSuiteUnit {
 
