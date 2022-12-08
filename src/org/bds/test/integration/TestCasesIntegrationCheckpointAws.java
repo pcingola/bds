ba8400c026f0edbd4e692ece177b8ecfb45ed7b8
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2022-12-08 17:59:59 -0500

    Project updated

--- src/org/bds/test/integration/TestCasesIntegrationCheckpointAws.java
+++ src/org/bds/test/integration/TestCasesIntegrationCheckpointAws.java
@@ -2,7 +2,6 @@ package org.bds.test.integration;
 
 import org.bds.data.DataS3;
 import org.bds.test.TestCasesBaseAws;
-import org.bds.util.Gpr;
 import org.junit.Test;
 
 /**
@@ -10,25 +9,29 @@ import org.junit.Test;
  */
 public class TestCasesIntegrationCheckpointAws extends TestCasesBaseAws {
 
-	public static boolean debug = false;
+    public static boolean debug = false;
 
-	/**
-	 * Test creating a remote checkpoint on AWS S3
-	 */
-	@Test
-	public void test29() {
-		runAndCheck("test/checkpoint_29.bds", "ok", true);
-	}
+    public TestCasesIntegrationCheckpointAws() {
+        dir = "test/integration/checkpoint_aws/";
+    }
 
-	/**
-	 * Test creating a checkpoint on AWS S3 and recovering from it
-	 */
-	@Test
-	public void test30() {
-		String bucket = awsBucketName();
-		String region = awsRegion();
-		String checkpointFile = "https://" + bucket + ".s3." + region + "." + DataS3.AWS_S3_VIRTUAL_HOSTED_DOMAIN + "/tmp/bds/checkpoint_30.chp";
-		runAndCheckpoint("test/checkpoint_30.bds", checkpointFile, "sum", 285, null);
-	}
+    /**
+     * Test creating a remote checkpoint on AWS S3
+     */
+    @Test
+    public void test29() {
+        runAndCheck(dir + "checkpoint_29.bds", "ok", true);
+    }
+
+    /**
+     * Test creating a checkpoint on AWS S3 and recovering from it
+     */
+    @Test
+    public void test30() {
+        String bucket = awsBucketName();
+        String region = awsRegion();
+        String checkpointFile = "https://" + bucket + ".s3." + region + "." + DataS3.AWS_S3_VIRTUAL_HOSTED_DOMAIN + "/tmp/bds/checkpoint_30.chp";
+        runAndCheckpoint(dir + "checkpoint_30.bds", checkpointFile, "sum", 285, null);
+    }
 
 }
