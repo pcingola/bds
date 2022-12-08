b9e777df5f4e530f63ef1fb0ea4be24e7d453129
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2022-12-08 06:26:44 -0500

    Project updated

--- src/org/bds/test/unit/TestCasesCustomResources.java
+++ src/org/bds/test/unit/TestCasesCustomResources.java
@@ -10,6 +10,10 @@ import org.junit.Test;
  */
 public class TestCasesCustomResources extends TestCasesBase {
 
+    public TestCasesCustomResources() {
+        dir = "test/task/custom_resources/";
+    }
+
     @Test
     public void test01_onlyOneTask() {
         var expectedOut = "Start task 0\n" +
@@ -18,7 +22,7 @@ public class TestCasesCustomResources extends TestCasesBase {
                 "End task 1\n" +
                 "Start task 2\n" +
                 "End task 2";
-        runAndCheckStdout("test/run_custom_resources_01.bds", expectedOut);
+        runAndCheckStdout(dir + "run_custom_resources_01.bds", expectedOut);
     }
 
     @Test
@@ -29,11 +33,11 @@ public class TestCasesCustomResources extends TestCasesBase {
                 "End\n" +
                 "End\n" +
                 "End";
-        runAndCheckStdout("test/run_custom_resources_02.bds", expectedOut);
+        runAndCheckStdout(dir + "run_custom_resources_02.bds", expectedOut);
     }
 
     @Test
     public void test03_notEnoughResources() {
-        runAndCheckError("test/run_custom_resources_03.bds", "Error in wait statement, file test/run_custom_resources_03.bds, line 13");
+        runAndCheckError(dir + "run_custom_resources_03.bds", "Error in wait statement, file test/task/custom_resources/run_custom_resources_03.bds, line 13");
     }
 }
