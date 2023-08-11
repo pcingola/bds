b797f621192b066b952e8fbaca70d18debd3c766
Oskar Korczak <oskar.korczak@protonmail.com>
2023-08-11 19:28:44 +0200

    feat: Add definition provider for `bds` language, improving hover functionality.

--- vscode/bds/extension.ts
+++ vscode/bds/extension.ts
@@ -2,18 +2,57 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
-    vscode.languages.registerHoverProvider("bds", {
-      provideHover(
-        document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken
-      ): vscode.ProviderResult<vscode.Hover> {
-        // Your logic to determine hover content
-        const hoverText = "Your Hover Content Here";
+    vscode.languages.registerDefinitionProvider(
+      "bds",
+      new (class implements vscode.DefinitionProvider {
+        provideDefinition(
+          document: vscode.TextDocument,
+          position: vscode.Position,
+          token: vscode.CancellationToken
+        ): vscode.ProviderResult<vscode.Definition> {
+          const word = document.getText(
+            document.getWordRangeAtPosition(position)
+          );
 
-        return new vscode.Hover(hoverText);
-      },
-    })
+          // Check if the user is hovering over a class instantiation like "new A()"
+          if (document.lineAt(position.line).text.includes(`new ${word}()`)) {
+            const regex = new RegExp(`class\\s+${word}\\s*{`);
+            for (let i = 0; i < document.lineCount; i++) {
+              const line = document.lineAt(i);
+              const match = line.text.match(regex);
+              if (match) {
+                const start = match.index || 0;
+                return new vscode.Location(
+                  document.uri,
+                  new vscode.Range(
+                    new vscode.Position(i, start),
+                    new vscode.Position(i, start + word.length)
+                  )
+                );
+              }
+            }
+          } else {
+            // Search for the variable or class declaration
+            const regex = new RegExp(`\\b${word}\\b\\s*=[^=]|class\\s+${word}`);
+            for (let i = position.line; i >= 0; i--) {
+              const line = document.lineAt(i);
+              const match = line.text.match(regex);
+              if (match) {
+                const start = match.index || 0;
+                return new vscode.Location(
+                  document.uri,
+                  new vscode.Range(
+                    new vscode.Position(i, start),
+                    new vscode.Position(i, start + word.length)
+                  )
+                );
+              }
+            }
+          }
+          return null;
+        }
+      })()
+    )
   );
 }
 
