d8de7bd867ab35832f550ff991c9644902cfce30
Oskar Korczak <oskar.korczak@protonmail.com>
2023-08-14 00:58:28 +0200

    feat: Refactor file indexing for improved performance and scalability

--- vscode/bds/server/src/fileIndexer.ts
+++ vscode/bds/server/src/fileIndexer.ts
@@ -1,68 +1,35 @@
 import { RemoteWorkspace, WorkspaceFolder } from "vscode-languageserver/node";
 import { TextDocument } from "vscode-languageserver-textdocument";
-import { readFileSync, readdirSync, statSync } from "fs";
-import { join, extname } from "path";
+import { promises as fs } from "fs";
 import { fileURLToPath } from "url";
 import { SymbolIndex } from "./symbolIndex";
+import { glob } from "glob";
 
-export function indexAllFilesInWorkspace(
+export async function indexAllFilesInWorkspace(
   workspace: RemoteWorkspace,
   supportsWorkspaceFolders: boolean | undefined,
-  symbols: SymbolIndex
-): void {
+  index: SymbolIndex
+): Promise<void> {
   if (supportsWorkspaceFolders) {
-    workspace.getWorkspaceFolders().then(processWorkspaceFolders(symbols));
+    const folders = await workspace.getWorkspaceFolders();
+    if (folders) {
+      for (const folder of folders) {
+        await indexFilesInFolder(folder, index);
+      }
+    }
   }
 }
 
-function processWorkspaceFolders(
-  symbols: SymbolIndex
-): (folders: WorkspaceFolder[] | null) => void {
-  return function (folders: WorkspaceFolder[] | null): void {
-    folders?.forEach(indexFilesInFolder(symbols));
-  };
-}
+async function indexFilesInFolder(
+  folder: WorkspaceFolder,
+  index: SymbolIndex
+): Promise<void> {
+  const folderPath = fileURLToPath(folder.uri);
+  const files = glob.sync(folderPath + "/**/*.bds");
 
-function indexFilesInFolder(
-  symbols: SymbolIndex
-): (folder: WorkspaceFolder) => void {
-  return function (folder: WorkspaceFolder): void {
-    const folderPath = fileURLToPath(folder.uri);
-    const targetFiles = findAllFilesWithExtensionInDir(folderPath, ".bds");
-    targetFiles.forEach(addFileToSymbolIndex(symbols));
-  };
-}
-
-function addFileToSymbolIndex(index: SymbolIndex): (filePath: string) => void {
-  return function (filePath: string): void {
-    const fileContent = readFileSync(filePath, "utf8");
+  for (const filePath of files) {
+    const fileContent = await fs.readFile(filePath, "utf8");
     const document = TextDocument.create(filePath, "bds", 1, fileContent);
     index.parseAndIndexDocument(document);
-  };
-}
-
-function findAllFilesWithExtensionInDir(
-  directory: string,
-  extension: string,
-  filesFound: string[] = []
-): string[] {
-  readdirSync(directory).forEach(
-    determineFileType(directory, filesFound, extension)
-  );
-  return filesFound;
-}
-
-function determineFileType(
-  directory: string,
-  filesFound: string[],
-  extension: string
-): (filename: string) => void {
-  return function (filename: string): void {
-    const fullPath = join(directory, filename);
-    if (statSync(fullPath).isDirectory()) {
-      findAllFilesWithExtensionInDir(fullPath, extension, filesFound);
-    } else if (extname(filename) === extension) {
-      filesFound.push(fullPath);
-    }
-  };
+  }
 }
