35297e38bad690c5601a8d87532cca70f59b3750
Oskar Korczak <oskar.korczak@protonmail.com>
2023-08-13 19:50:31 +0200

    refactor: Refactor file indexer imports and arrow function syntax

--- vscode/bds/server/src/fileIndexer.ts
+++ vscode/bds/server/src/fileIndexer.ts
@@ -1,8 +1,8 @@
 import { RemoteWorkspace, WorkspaceFolder } from "vscode-languageserver/node";
+import { TextDocument } from "vscode-languageserver-textdocument";
 import { readFileSync, readdirSync, statSync } from "fs";
 import { join, extname } from "path";
 import { fileURLToPath } from "url";
-import { TextDocument } from "vscode-languageserver-textdocument";
 import { SymbolIndex } from "./symbolIndex";
 
 export function indexAllBDSFiles(
@@ -12,9 +12,9 @@ export function indexAllBDSFiles(
 ) {
   if (hasWorkspaceFoldersCapability) {
     workspace.getWorkspaceFolders().then((folders) => {
-      folders?.forEach((folder) => {
-        indexBDSFilesInWorkspace(folder, symbolindex);
-      });
+      folders?.forEach((folder) =>
+        indexBDSFilesInWorkspace(folder, symbolindex)
+      );
     });
   }
 }
@@ -22,6 +22,7 @@ export function indexAllBDSFiles(
 function indexBDSFilesInWorkspace(folder: WorkspaceFolder, index: SymbolIndex) {
   const folderUri = fileURLToPath(folder.uri);
   const bdsFiles = findAllBDSFiles(folderUri);
+
   bdsFiles.forEach((file) => {
     const content = readFileSync(file, "utf8");
     const document = TextDocument.create(file, "bds", 1, content);
@@ -30,14 +31,15 @@ function indexBDSFilesInWorkspace(folder: WorkspaceFolder, index: SymbolIndex) {
 }
 
 function findAllBDSFiles(dir: string, fileList: string[] = []): string[] {
-  const files = readdirSync(dir);
-  for (const file of files) {
+  readdirSync(dir).forEach((file) => {
     const filePath = join(dir, file);
+
     if (statSync(filePath).isDirectory()) {
-      fileList = findAllBDSFiles(filePath, fileList);
+      findAllBDSFiles(filePath, fileList);
     } else if (extname(file) === ".bds") {
       fileList.push(filePath);
     }
-  }
+  });
+
   return fileList;
 }
